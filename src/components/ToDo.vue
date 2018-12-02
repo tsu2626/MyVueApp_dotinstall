fc3d2c4fce8550d486fed538423e5dcd72304740
tsu2626 <dmwtpdmwtp24@gmail.com>
2018-12-02 11:38:18 +0900

    create_deleteButton&fix_design

--- src/components/ToDo.vue
+++ src/components/ToDo.vue
@@ -1,8 +1,11 @@
 <template>
   <div class="hello">
-    <h1>My Todos</h1>
+    <h1 class=title>My Todos</h1>
     <ul>
-      <li v-for="todo in todos" :key="todo">{{ todo }}</li>
+      <li v-for="(todo, index) in todos" :key="todo">
+        {{ todo }}
+        <span @click="deleteItem(index)" class="delete">[削除]</span>
+      </li>
     </ul>
     <form @submit.prevent="addItem">
       <input type="text" v-model="newItem">
@@ -12,25 +15,30 @@
 </template>
 
 <script>
-export default {
-  name: 'ToDo',
-  newItem: "",
-  data () {
-    return {
-      todos: [
-        'task1',
-        'task2',
-        'task3'
-      ]
-    }
-  },
-  methods: {
-    addItem: function() {
-      this.todos.push(this.newItem);
-      this.newItem = ""
+  export default {
+    name: 'ToDo',
+    newItem: "",
+    data () {
+      return {
+        todos: [
+          'task1',
+          'task2',
+          'task3'
+        ]
+      }
+    },
+    methods: {
+      addItem: function() {
+        this.todos.push(this.newItem);
+        this.newItem = ""
+      },
+      deleteItem: function(index) {
+        if(confirm('Are You Sure?')){
+          this.todos.splice(index, 1);
+        }
+      }
     }
   }
-}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
